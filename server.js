ebe4de7c44010907d09bbb18d3c7b43ea4dba7f9
leili <476371581@qq.com>
2019-09-02 18:01:42 +0800

    a

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 let http = require('http');
 let users = [
-    {name:'Allen',value:11}
+    {name:'Allen',value:121}
 ];
 let server = http.createServer((req,res)=>{
     res.setHeader("Access-Control-Allow-Origin","*");
-    //允许的header类型
     res.setHeader("Access-Control-Allow-Headers","content-type");
-    //跨域允许的请求方式 
     res.setHeader("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
     
     if(req.url === '/api/users'){
